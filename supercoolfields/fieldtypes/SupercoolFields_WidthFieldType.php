b91ff3456e9c7d8859e829e215c598f17a97108a
Josh Angell <josh@supercooldesign.co.uk>
2014-10-10 16:09:21 +0100

    add custom settings and therefore default values to width

--- supercoolfields/fieldtypes/SupercoolFields_WidthFieldType.php
+++ supercoolfields/fieldtypes/SupercoolFields_WidthFieldType.php
@@ -57,16 +57,88 @@ class SupercoolFields_WidthFieldType extends BaseOptionsFieldType
     ));
   }
 
+
+	/**
+	* @inheritDoc BaseElementFieldType::getSettingsHtml()
+	*
+	* @return string|null
+	*/
+	public function getSettingsHtml()
+	{
+		$options = $this->getOptions();
+
+		if (!$options)
+		{
+			// Give it a default row
+			$options = array(
+				array(
+					'label' => 'One Third',
+					'value' => 'one-third'
+				),
+				array(
+					'label' => 'Two Thirds',
+					'value' => 'two-thirds',
+					'default' => true
+				),
+				array(
+					'label' => 'Full Width',
+					'value' => 'one-whole'
+				)
+			);
+		}
+
+		return craft()->templates->renderMacro('_includes/forms', 'editableTableField', array(
+			array(
+				'label'        => $this->getOptionsSettingsLabel(),
+				'instructions' => Craft::t('Define the available options.'),
+				'id'           => 'options',
+				'name'         => 'options',
+				'addRowLabel'  => Craft::t('Add an option'),
+				'cols'         => array(
+					'label' => array(
+						'heading'      => Craft::t('Option Label'),
+						'type'         => 'singleline',
+						'autopopulate' => 'value'
+					),
+					'value' => array(
+						'heading'      => Craft::t('Value'),
+						'type'         => 'singleline',
+						'class'        => 'code'
+					),
+					'default' => array(
+						'heading'      => Craft::t('Default?'),
+						'type'         => 'checkbox',
+						'class'        => 'thin'
+					),
+				),
+				'rows' => $options
+			)
+		));
+	}
+
 	// Protected Methods
 	// =========================================================================
 
 	/**
-	 * @inheritDoc BaseOptionsFieldType::getOptionsSettingsLabel()
-	 *
-	 * @return string
-	 */
+	* @inheritDoc BaseOptionsFieldType::getOptionsSettingsLabel()
+	*
+	* @return string
+	*/
 	protected function getOptionsSettingsLabel()
 	{
 		return Craft::t('Width Options');
 	}
+
+	/**
+	* @inheritDoc BaseSavableComponentType::defineSettings()
+	*
+	* @return array
+	*/
+	protected function defineSettings()
+	{
+		return array(
+			'options' => array(AttributeType::Mixed, 'default' => array())
+		);
+	}
+
 }
